55bf3af46bb2aa89856f0092124e5dca33a2feec
MarinaMcGrath <marinamcgrath92@gmail.com>
2017-09-14 20:56:53 -0500

    [add]map styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ import { Events } from 'ionic-angular';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage = ManualEntryPage;
+  rootPage = LoginPage;
   pages: Array<{title: string, component: any}>;
 
   constructor(
